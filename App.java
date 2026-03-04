9f3bc60d8e67979bb9a8ad960e66eb7879508268
Antón Faraldo Mosquera <anton.estudios.11@gmail.com>
2026-03-04 11:01:28 +0100

    Implementar vista FXML y GameController con corrección de naming

---
+++ App.java
@@ -0,0 +1,33 @@
+package dam.proyectofinal.afm;
+
+import dam.proyectofinal.afm.model.Dificultad;
+import dam.proyectofinal.afm.model.Tablero;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+//	public static void main(String[] args) {
+//		// Dificultad de prueba (8x8 con 10 minas)
+//		Dificultad prueba = new Dificultad();
+//		prueba.setFilas(8);
+//		prueba.setColumnas(8);
+//		prueba.setNumMinas(10);
+//		
+//		// Creación del Tablero
+//		Tablero pruebaTablero = new Tablero(prueba);
+//		
+//		// Mostramos el resultado
+//		System.out.println("---Prueba de Tablero Buscaminas---");
+//		pruebaTablero.imprimirTableroConsola();
+//	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}

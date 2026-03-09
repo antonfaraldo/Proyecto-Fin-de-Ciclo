f8e8df0cb26a9a217591495446a7a5f56ba83369
Antón Faraldo Mosquera <anton.estudios.11@gmail.com>
2026-03-09 17:46:15 +0100

    Implementar alerta de partida personalizada y arreglo visual

--- src/main/java/dam/proyectofinal/afm/controller/PersonalizarNivelController.java
+++ src/main/java/dam/proyectofinal/afm/controller/PersonalizarNivelController.java
@@ -1,8 +1,13 @@
 package dam.proyectofinal.afm.controller;
 
+import java.util.Optional;
+
 import dam.proyectofinal.afm.util.AppShell;
 import dam.proyectofinal.afm.util.View;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
@@ -20,21 +25,31 @@ public class PersonalizarNivelController {
 	
 	@FXML 
 	private void handleJugar() {
-		// Se obtienen los valores seleccionados por el usuario
-		int f = spinFilas.getValue();
-		int c = spinColumnas.getValue();
-		int m = spinMinas.getValue();
+		// Alerta de Información
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("Nivel Personalizada");
+		alert.setHeaderText("Aviso de Ranking");
+		alert.setContentText("Has seleccionado un nivel personalizado. Ten en cuenta que estas partidas NO se guardarán en el ranking global.\n\n¿Deseas continuar?");
 		
-		// Calculamos el límite de seguridad (las minas no pueden ser más del 80% del tablero)
-		int maxMinasPermitidas = (int) ((f * c) * 0.8);
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
 		
-		// Validacion de seguridad
-		if (m > maxMinasPermitidas) {
-			m = maxMinasPermitidas;
+			// Se obtienen los valores seleccionados por el usuario
+			int f = spinFilas.getValue();
+			int c = spinColumnas.getValue();
+			int m = spinMinas.getValue();
+			
+			// Calculamos el límite de seguridad (las minas no pueden ser más del 80% del tablero)
+			int maxMinasPermitidas = (int) ((f * c) * 0.8);
+			
+			// Validacion de seguridad
+			if (m > maxMinasPermitidas) {
+				m = maxMinasPermitidas;
+			}
+			// Se carga la vista
+			GameController game = (GameController) AppShell.getInstance().loadView(View.GAME);
+	        game.prepararPartidaPersonalizada(f, c, m);
 		}
-		// Se carga la vista
-		GameController game = (GameController) AppShell.getInstance().loadView(View.GAME);
-        game.prepararPartidaPersonalizada(f, c, m);
 	}
 	
 	@FXML

ec9b8b8ccd0615ca5d4f71c6ea99544a45e4ef7e
Antón Faraldo Mosquera <anton.estudios.11@gmail.com>
2026-03-13 09:24:53 +0100

    Restringir entrada de letras en los spinners de nivel personalizado

--- src/main/java/dam/proyectofinal/afm/controller/PersonalizarNivelController.java
+++ src/main/java/dam/proyectofinal/afm/controller/PersonalizarNivelController.java
@@ -4,12 +4,15 @@ import java.util.Optional;
 
 import dam.proyectofinal.afm.util.AppShell;
 import dam.proyectofinal.afm.util.View;
+import javafx.collections.ListChangeListener.Change;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 
 public class PersonalizarNivelController {
 	@FXML private Spinner<Integer> spinFilas;
@@ -21,8 +24,29 @@ public class PersonalizarNivelController {
 		spinFilas.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(5, 30, 10));
 		spinColumnas.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(5, 50, 10));
         spinMinas.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 500, 15));
+        
+        // Validación para evitar letras
+        configurarValidacionNumerica(spinFilas);
+        configurarValidacionNumerica(spinColumnas);
+        configurarValidacionNumerica(spinMinas);
 	}
 	
+	private void configurarValidacionNumerica(Spinner<Integer> spinner) {
+		// TODO Auto-generated method stub
+		spinner.setEditable(true); // Se permite editar los valores
+		
+		// Editor de texto
+		TextField editor = spinner.getEditor();
+		
+		// Filtro que solo permite numeros
+		editor.setTextFormatter(new TextFormatter<>(change -> {
+			if (change.getControlNewText().matches("\\d*")) {
+				return change; // Si es numero se valida
+			}
+			return null; // Si es una letra o simbolo mo se valida 
+		} ));
+	}
+
 	@FXML 
 	private void handleJugar() {
 		// Alerta de Información
